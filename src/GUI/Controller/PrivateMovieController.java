563a6158e6f319895fb6c34a40c0a4cc326d7130
SCRMMASTER <113671728+SCRMMASTER@users.noreply.github.com>
2023-01-04 10:19:05 +0100

    GUI VIDERE

--- src/GUI/Controller/PrivateMovieController.java
+++ src/GUI/Controller/PrivateMovieController.java
@@ -1,9 +1,28 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+
 public class PrivateMovieController extends BaseController {
 
     @Override
     public void setup() {
 
     }
+
+    public void addMovie(ActionEvent actionEvent) {
+        System.out.println("Add movie");
+    }
+
+    public void deleteMovie(ActionEvent actionEvent) {
+        System.out.println("Delete Movie");
+    }
+
+    public void addCategory(ActionEvent actionEvent) {
+        System.out.println("AddCategory");
+    }
+
+    public void deleteCategory(ActionEvent actionEvent) {
+        System.out.println("dekete categary");
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
         primaryStage.setTitle("Movie Collection");
         primaryStage.setScene(new Scene(root));
         primaryStage.setMaxHeight(600);
-        primaryStage.setMaxWidth(800);
+        primaryStage.setMaxWidth(700);
         primaryStage.show();
     }
 
