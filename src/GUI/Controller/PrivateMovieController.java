787fd4b763fd6f2b0c45998fdd4de327ed152df6
brunstighanhval <mlkaer1@hotmail.com>
2023-01-04 11:21:48 +0100

    Controller and View Movie and Category

--- src/GUI/Controller/PrivateMovieController.java
+++ src/GUI/Controller/PrivateMovieController.java
@@ -1,28 +1,99 @@
 package GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
 
 public class PrivateMovieController extends BaseController {
+    @FXML
+    public ListView lstCategory;
+    public TableView tblMovie;
+    @FXML
+    private Button btnaddCategory, btndeleteCategory, btnaddMovie, btndeleteMovie;
 
     @Override
     public void setup() {
 
     }
 
-    public void addMovie(ActionEvent actionEvent) {
+    public void handeladdMovie(ActionEvent actionEvent) throws IOException {
         System.out.println("Add movie");
+        // Finds where the fxml is located.
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GUI/View/NewMovieView.fxml"));
+        // Loads the stage.
+        Parent root = fxmlLoader.load();
+        // Makes the new stage.
+        Stage stage = new Stage();
+        // Title of the stage<
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Add new movie");
+        stage.setScene(new Scene(root));
+        // The stage is then displayed and the program waits for
+        // the user to interact with the delete song dialog.
+        stage.showAndWait();
     }
 
-    public void deleteMovie(ActionEvent actionEvent) {
+    public void handeldeleteMovie(ActionEvent actionEvent) {
         System.out.println("Delete Movie");
+        try {
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setHeaderText("Are you sure you wanna delete this movie?");
+            alert.showAndWait();
+            if(alert.getResult() == ButtonType.OK) {
+                System.out.println("you have now deleted the movie");
+               // movieModel.deleteSong(movieModel.getSelectedSong());
+            }
+        } catch (Exception e){
+            //displayError(e);
+
+            e.printStackTrace();
+        }
     }
 
-    public void addCategory(ActionEvent actionEvent) {
+    public void handeladdCategory(ActionEvent actionEvent) throws IOException {
         System.out.println("AddCategory");
+        // Finds where the fxml is located.
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GUI/View/NewCategoryView.fxml"));
+        // Loads the stage.
+        Parent root = fxmlLoader.load();
+        // Makes the new stage.
+        Stage stage = new Stage();
+        // Title of the stage<
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Add new category");
+        stage.setScene(new Scene(root));
+        // The stage is then displayed and the program waits for
+        // the user to interact with the delete song dialog.
+        stage.showAndWait();
     }
 
-    public void deleteCategory(ActionEvent actionEvent) {
-        System.out.println("dekete categary");
+    public void handeldeleteCategory(ActionEvent actionEvent) {
+        System.out.println("delete categary");
+        try {
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setHeaderText("Are you sure you wanna delete this category?");
+            alert.showAndWait();
+            if(alert.getResult() == ButtonType.OK) {
+                System.out.println("you have now deleted the category");
+                // movieModel.deleteSong(movieModel.getSelectedSong());
+            }
+        } catch (Exception e){
+            //displayError(e);
+
+            e.printStackTrace();
+        }
     }
+
+
 
 }
