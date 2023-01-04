d16ea9427da67266efc01729a145ad4e80c2520f
JSM80 <mail@moellers-hule.dk>
2023-01-04 11:28:58 +0100

    Category Stuff

--- src/BLL/CategoryManager.java
+++ src/BLL/CategoryManager.java
@@ -1,4 +1,40 @@
 package BLL;
 
+import BE.Category;
+import DAL.ICategoryDataAccess;
+import DAL.db.CategoryDAO_DB;
+
+import java.util.List;
+
 public class CategoryManager {
+
+    private ICategoryDataAccess CategoryDAO_DB;
+
+    public CategoryManager(){
+
+        CategoryDAO_DB = new CategoryDAO_DB();
+
+    }
+
+    //Retrieve alle Categories from database table
+    public List<Category> getAllCategories() throws Exception
+    {
+        return CategoryDAO_DB.getAllCategories();
+
+    //Create new category(Genre)
+
+    }
+    public Category createNewCategory(int id, String Genre) throws Exception
+    {
+        return CategoryDAO_DB.createCategory(id, Genre);
+
+    }
+    //Delete Category(genre)
+
+    public void deletePlaylist(Category selectedCategory) throws Exception {
+
+        CategoryDAO_DB.deleteCategory(selectedCategory);
+    }
+
+
 }
