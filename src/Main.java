65cefc9c02940e8cc9c48e23fe0d3587154c5d9c
SCRMMASTER <113671728+SCRMMASTER@users.noreply.github.com>
2023-01-17 01:16:19 +0100

    FRISK STYLING HOT JINGLE

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import javax.sound.sampled.AudioInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class Main extends Application {
 
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/GUI/View/PrivateMovieView.fxml"));
         Parent root = loader.load();
@@ -32,10 +39,30 @@ public class Main extends Application {
         primaryStage.setResizable(false);
 
         primaryStage.show();
+        jinglePlay().play();
+
+
+
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(args);
     }
+
+    private AudioClip jingle = null;
+
+    public AudioClip jinglePlay() {
+
+        if (jingle == null) {
+
+            String src = getClass().getResource("jingle.mp3").toString();
+            jingle = new AudioClip(src);
+
+
+        }
+        return jingle;
+
+    }
 }
+
+
